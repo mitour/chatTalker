7c93a2dec80515d9166a75cb4b42bfe067601de2
Mitour <love2082020@gmail.com>
2022-06-05 20:56:28 +0800

    dynamic content on different case options

--- js/all.js
+++ js/all.js
@@ -1,6 +1,66 @@
-// https://hackmd.io/itEybVN0S9uUxiNbpY7-Xw
-// Initialize Swiper
+$(document).ready(function () {
+  // Initialize Swiper
+  initSwiper();
+
+  // case
+  // https://github.com/iamAmberhh/chatTalker/blob/master/js/charge.js
+  const caseOptions = document.querySelector("#caseOptions");
+  const subsCount = document.querySelectorAll(".subsCount"); // [0]:基本訂閱數、[1]:標準訂閱數
+  const bacisFee = document.querySelector(".bacisFee");
+  const standardFee = document.querySelector(".standardFee");
+
+  const plan = [
+    {
+      planName: "10000位",
+      subsCount: 10000,
+      bacisFee: 600,
+      standardFee: 1600,
+    },
+    {
+      planName: "15000位",
+      subsCount: 15000,
+      bacisFee: 500,
+      standardFee: 1500,
+    },
+    {
+      planName: "20000位",
+      subsCount: 20000,
+      bacisFee: 400,
+      standardFee: 1400,
+    },
+    {
+      planName: "25000位",
+      subsCount: 25000,
+      bacisFee: 300,
+      standardFee: 1300,
+    },
+    {
+      planName: ">25000位",
+      subsCount: ">25000",
+      bacisFee: 200,
+      standardFee: 1200,
+    },
+  ];
+
+  // toggle item
+  $(".caseItem").click(function () {
+    $(this).toggleClass("active");
+    $(this).siblings().removeClass("active");
+  });
+  // dynamic content
+  caseOptions.addEventListener("click", function (e) {
+    plan.forEach((item) => {
+      if (e.target.textContent === item.planName) {
+        subsCount[0].textContent = item.subsCount;
+        subsCount[1].textContent = item.subsCount;
+        bacisFee.textContent = item.bacisFee;
+        standardFee.textContent = item.standardFee;
+      }
+    });
+  });
+});
 function initSwiper() {
+  // https://hackmd.io/itEybVN0S9uUxiNbpY7-Xw
   let swiper = new Swiper(".swiper", {
     /* 斷點設定 */
     breakpoints: {
@@ -28,4 +88,3 @@ function initSwiper() {
     },
   });
 }
-initSwiper();
